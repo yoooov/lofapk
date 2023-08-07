84353c4372476a07d6f187e3246a4bd34ed725cb
YoV <yoann.vidil.ext@valeo.com>
2023-08-07 14:37:25 +0200

    some xml resources

---
+++ app/src/main/java/com/valeo/app/lofapk/utils/ApiConstant.kt
@@ -0,0 +1,26 @@
+package com.valeo.app.lofapk.utils
+
+class ApiConstant {
+
+    companion object {
+
+        const val BASE_URL = "https://iss1-sv00052.vnet.valeo.com/"
+        const val LOGIN_URL = "oauth/token"
+        const val POSTS_URL = "escheduling/of-location_test/"
+
+        const val SEPA = "/"
+        const val SPACER = " "
+        const val MAIN_TITLE = "LOFPDA"
+
+        const val CLIENT_ID = "5n7f0v7VMDa9cWKIcprgOA.."
+        const val CLIENT_SECRET = "ICVI_JTcG28upnZ3howHSw.."
+
+        const val REF_EMP_API_BASEURL = "https://iss1-sv00052.vnet.valeo.com/escheduling/"
+        const val REQUEST_REFRESH_TOKEN = "/oauth/token"
+
+        // testing purpose
+        const val ACCESS_TOKEN = "KnN58XiB8UC45EU9X1EBfg"
+        const val CLIENT_B64 = "NW43ZjB2N1ZNRGE5Y1dLSWNwcmdPQS4uOklDVklfSlRjRzI4dXBuWjNob3dIU3cuLg=="
+
+    }
+}
